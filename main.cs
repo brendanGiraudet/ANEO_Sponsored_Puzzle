f0a6b85a03f940cae00e51e93b30ded00e6087a7
Brendan GIRAUDET <brendan.GIRAUDET@ebp.com>
2019-07-03 13:48:49 +0200

    premier jet d'algo

--- main.cs
+++ main.cs
@@ -15,28 +15,43 @@ class Solution
     {
         int maxSpeed = int.Parse(Console.ReadLine());// km/h
         int lightCount = int.Parse(Console.ReadLine());
-        var Feux = new List<Feu>();
+        var fires = new List<Fire>();
         for (int i = 0; i < lightCount; i++)
         {
             string[] inputs = Console.ReadLine().Split(' ');
-            Feux.Add(new Feu
+            var fire = new Fire
             {
                 Distance = int.Parse(inputs[0]),
                 Duration = int.Parse(inputs[1])
-            });
+            };
+            
+            for (int speed = maxSpeed; speed >= 0; speed--)
+            {
+                if (CanCross(fire, speed))
+                {
+                    fire.Speeds.Add(speed);
+                }
+            }
+            fires.Add(fire);
         }
-        Feux.ForEach(f => { Console.Error.WriteLine(f); });
+        fires.ForEach(f => { Console.Error.WriteLine(f); });
+        
         Console.WriteLine("answer");
     }
+
+    static bool CanCross(Fire feu, int speed)
+    {
+        return true;
+    }
 }
-class Feu
+class Fire
 {
     public int Distance { get; set; } // en mettre
     public int Duration { get; set; } // en secondes
-    public List<int> Vitesse { get; set; } = new List<int>();
+    public List<int> Speeds { get; set; } = new List<int>();
 
     public override string ToString()
     {
-        return "Dist : " + Distance + "m Dur : " + Duration + "s";
+        return "Dist : " + Distance + "m Dur : " + Duration + "s Speed : " + string.Join(',', Speeds) ;
     }
 }
